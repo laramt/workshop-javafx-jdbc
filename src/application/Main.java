444fce346c11583708a7ac2d4c5773627fb4538d
Lara Menck <lara.menckt@gmail.com>
2022-03-18 12:01:31 -0300

    MainViewController

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
